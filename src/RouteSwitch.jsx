b1b2869cd20c288e2116543c01b0b20f15fce057
ruipsmartins <rui_pedru@hotmail.com>
2022-10-19 15:15:01 +0100

    routers links and pages

---
+++ src/RouteSwitch.jsx
@@ -0,0 +1,33 @@
+import { Link, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import NavBar from "./Components/navBar";
+import Trabalhos from "./Pages/trabalhos";
+
+const RouteSwitch = () => {
+  return (
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/trabalhos" element={<Trabalhos />} />
+      </Routes>
+    </>
+  );
+};
+
+export default RouteSwitch;
+
+{
+  /* <BrowserRouter>
+  <Routes>
+    <Route path="/" element={<App />}>
+      <Route index element={<Home />} />
+      <Route path="teams" element={<Teams />}>
+        <Route path=":teamId" element={<Team />} />
+        <Route path="new" element={<NewTeamForm />} />
+        <Route index element={<LeagueStandings />} />
+      </Route>
+    </Route>
+  </Routes>
+</BrowserRouter>; */
+}
